9328954ab713d84543cbcb49d6d1799a6d8264d4
Darren Xu <darrenxu1994@yahoo.com.au>
2020-04-04 20:38:53 +1100

    Initial commit

---
+++ src/components/PageLanding.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div v-for="item in LandingRecipies" :key="item.id">
+      <RecipieCard :recipie="item" />
+    </div>
+  </div>
+</template>
+
+<script>
+import LandingRecipies from "../mock/LandingRecipies.json";
+// import RecipieCard from "@components/PageLandingComponents/ReceipieCard";
+import RecipieCard from "../components/PageLandingComponents/RecipieCard";
+
+export default {
+  name: "PageLanding",
+  components: {
+    RecipieCard
+  },
+  data() {
+    return {
+      LandingRecipies: LandingRecipies
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <PageLanding />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import PageLanding from "@/components/PageLanding.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
+    PageLanding
   }
-}
+};
 </script>
