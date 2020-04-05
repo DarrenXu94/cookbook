c8908c585fe16c95084e9a1f2e4efd08c28fc208
Darren Xu <darrenxu1994@yahoo.com.au>
2020-04-05 20:05:00 +1000

    Recipie page design

--- src/views/Recipie.vue
+++ src/views/Recipie.vue
@@ -30,7 +30,16 @@
         >{{ item }}</div>
       </section>
     </section>
-    <section class="recipie__directions">Directions</section>
+    <section class="recipie__directions">
+      <header class="recipie__directions-header">Directions</header>
+      <section class="recipie__directions">
+        <div
+          class="recipie__directions-item"
+          v-for="(item, index) in Directions"
+          :key="index"
+        >{{ item }}</div>
+      </section>
+    </section>
   </div>
 </template>
 
@@ -41,6 +50,7 @@ export default {
   name: "Recipie",
   mounted() {
     this.RecipiePage = this.RecipiePage.split("\n");
+    this.Directions = this.Directions.split("\n");
   },
   data() {
     return {
@@ -57,7 +67,14 @@ Two (6.5-ounce; 184g) cans chopped or minced clams, clams and liquid divided
 1 teaspoon (5ml) soy sauce
 Kosher salt
 Celery leaves (pale yellow-green leaves from the celery heart), for garnish
-Lemon wedges, for serving`
+Lemon wedges, for serving`,
+      Directions: `1. In a 12-inch stainless steel skillet, melt 2 tablespoons of butter over medium heat, and swirl pan until foaming subsides. Add celery, shallots, half the parsley (1/4 cup; 15g), garlic, Thai chili (if using), and a couple healthy grinds of black pepper to pan and cook, stirring occasionally, until vegetables are soft and mixture is fragrant, 1 to 2 minutes. Add bottled clam juice along with liquid from canned clams but not the clam meat. Increase heat to high and bring mixture to a boil, then reduce heat to a simmer until liquid is slightly reduced and vegetables have infused broth, about 5 minutes—there should be enough liquid in the pan to fully submerge the pound of cooked pasta. Turn off heat and add clam meat and soy sauce.
+
+2. In a pot of salted, boiling water, cook spaghetti, stirring frequently, until about 4 minutes shy of the way you like your pasta cooked (some would say "al dente"). When spaghetti is almost at that point of doneness, bring pan of clam sauce to a boil over high heat.
+
+3. Using tongs, transfer spaghetti to pan with clam sauce, distributing pasta so it is mostly submerged in sauce. Cook, tossing and stirring occasionally, until pasta is cooked to your liking and sauce has thickened slightly, about 4 minutes. Taste for seasoning, and add salt if necessary.
+
+4. Turn off heat, add remaining butter and parsley, and toss and stir spaghetti vigorously to incorporate parsley and emulsify butter into sauce. Divide pasta among four serving plates, spooning sauce and clams evenly over each portion. Garnish with celery leaves, and serve immediately, passing lemon wedges, for squeezing, at the table.`
     };
   }
 };
@@ -86,14 +103,14 @@ section.recipie__ingredients {
 }
 section.recipie__directions {
   grid-area: directions;
-  background: #202020;
+  background: white;
 }
 .recipie {
   display: grid;
-  grid-template-columns: 0.5fr 1fr 2fr 0.5fr;
+  grid-template-columns: 0.25fr 1fr 2fr 0.25fr;
   grid-template-rows: 3fr 0.5fr 3fr;
   grid-template-areas:
-    "recipie_image recipie_image   recipie_image recipie_image"
+    "blank_area recipie_image   recipie_image blank_area_end"
     "blank_area info_bar        info_bar blank_area_end"
     "blank_area ingredients     directions blank_area_end";
 }
@@ -115,6 +132,8 @@ section.recipie__directions {
 
 .recipie__ingredients-header {
   font-size: 1.7rem;
+  color: #ad7d52;
+  font-weight: bold;
 }
 
 .recipie__ingredients_body {
@@ -122,6 +141,25 @@ section.recipie__directions {
 }
 
 .recipie__ingredients_body-item {
+  padding-bottom: 1rem;
+}
+
+.recipie__directions {
+  padding: 1rem;
+}
+
+.recipie__directions-header {
+  font-size: 1.7rem;
+  color: #ad7d52;
+  font-weight: bold;
+}
+
+.recipie__directions_body {
+  margin-top: 1rem;
+}
+
+.recipie__directions-item {
+  font-size: 1.2rem;
   padding-bottom: 1rem;
 }
 
