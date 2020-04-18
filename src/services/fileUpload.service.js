1d23d5322bd1e9ae5da093df799397fda29a43c9
Darren Xu <darrenxu1994@yahoo.com.au>
2020-04-19 08:54:50 +1000

    Fetching individual data

--- src/services/fileUpload.service.js
+++ src/services/fileUpload.service.js
@@ -5,7 +5,11 @@ const BASE_URL = 'http://localhost:5000';
 function fetchAll() {
     const url = `${BASE_URL}/api/recipies`;
     return axios.get(url).then(x => x.data)
+}
 
+function fetchById(id) {
+    const url = `${BASE_URL}/api/recipies/${id}`;
+    return axios.get(url).then(x => x.data)
 }
 
 function upload(formData) {
@@ -18,4 +22,4 @@ function upload(formData) {
     //     img, { url: `${BASE_URL}/images/${img.id}` })));
 }
 
-export { upload, fetchAll }
+export { upload, fetchAll, fetchById }
--- src/views/Recipie.vue
+++ src/views/Recipie.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="recipie">
-    <div class="recipie__image"></div>
+    <div id="recipie__image" class="recipie__image"></div>
     <section class="blank"></section>
 
     <section class="recipie__info_bar layout">
       <section class="recipie__info_item layout-item-33">
         <div class="recipie__info_item_heading">Time</div>
-        <div class="recipie__info_item_value">15 mins</div>
+        <div class="recipie__info_item_value">{{time}} mins</div>
       </section>
       <section class="recipie__info_item layout-item-33">
         <div class="recipie__info_item_heading">Rating</div>
@@ -26,7 +26,7 @@
       </section>
       <section class="recipie__info_item layout-item-33">
         <div class="recipie__info_item_heading">Cost</div>
-        <div class="recipie__info_item_value">$15.50</div>
+        <div class="recipie__info_item_value">${{cost}}</div>
       </section>
     </section>
 
@@ -37,9 +37,10 @@
       <section class="recipie__ingredients_body">
         <div
           class="recipie__ingredients_body-item"
-          v-for="(item, index) in RecipiePage"
+          v-for="(item, index) in ingredients"
           :key="index"
         >{{ item }}</div>
+        <!-- <div class="recipie__ingredients_body-item">{{ ingredients }}</div> -->
       </section>
     </section>
     <section class="recipie__directions">
@@ -47,7 +48,7 @@
       <section class="recipie__directions">
         <div
           class="recipie__directions-item"
-          v-for="(item, index) in Directions"
+          v-for="(item, index) in directions"
           :key="index"
         >{{ item }}</div>
       </section>
@@ -56,38 +57,33 @@
 </template>
 
 <script>
-import RecipiePage from "../mock/RecipiePage.json";
+import { fetchById } from "../services/fileUpload.service";
 
 export default {
   name: "Recipie",
-  mounted() {
-    this.RecipiePage = this.RecipiePage.split("\n");
-    this.Directions = this.Directions.split("\n");
+  async mounted() {
+    // console.log(this.$route);
+    const res = await fetchById(this.$route.params.id);
+    const data = res[0];
+    console.log(data);
+    this.rating = data.rating;
+    this.time = data.time;
+    this.cost = data.cost;
+    this.ingredients = data.ingredients.split("\n");
+    this.directions = data.directions.split("\n");
+
+    const imgHoverDiv = document.getElementById(`recipie__image`);
+    const img = "url(" + data.image + ")";
+    imgHoverDiv.style.backgroundImage = img;
+    imgHoverDiv.style.backgroundPosition = "center";
   },
   data() {
     return {
-      RecipiePage: `4 tablespoons (60g) unsalted butter, cut into 4 pieces and divided
-2 celery ribs (5 oz; 140g), peeled (optional) and cut into small dice
-2 medium shallots (60g), minced
-1/2 cup (30g) finely chopped fresh parsley leaves, divided
-4 medium garlic cloves (15g), finely minced
-1 fresh green Thai chili (4g), thinly sliced (optional, see note)
-Freshly ground black pepper
-Two (8-ounce; 237ml) bottles clam juice
-Two (6.5-ounce; 184g) cans chopped or minced clams, clams and liquid divided
-1 lb (450g) spaghetti
-1 teaspoon (5ml) soy sauce
-Kosher salt
-Celery leaves (pale yellow-green leaves from the celery heart), for garnish
-Lemon wedges, for serving`,
-      Directions: `1. In a 12-inch stainless steel skillet, melt 2 tablespoons of butter over medium heat, and swirl pan until foaming subsides. Add celery, shallots, half the parsley (1/4 cup; 15g), garlic, Thai chili (if using), and a couple healthy grinds of black pepper to pan and cook, stirring occasionally, until vegetables are soft and mixture is fragrant, 1 to 2 minutes. Add bottled clam juice along with liquid from canned clams but not the clam meat. Increase heat to high and bring mixture to a boil, then reduce heat to a simmer until liquid is slightly reduced and vegetables have infused broth, about 5 minutes—there should be enough liquid in the pan to fully submerge the pound of cooked pasta. Turn off heat and add clam meat and soy sauce.
-
-2. In a pot of salted, boiling water, cook spaghetti, stirring frequently, until about 4 minutes shy of the way you like your pasta cooked (some would say "al dente"). When spaghetti is almost at that point of doneness, bring pan of clam sauce to a boil over high heat.
-
-3. Using tongs, transfer spaghetti to pan with clam sauce, distributing pasta so it is mostly submerged in sauce. Cook, tossing and stirring occasionally, until pasta is cooked to your liking and sauce has thickened slightly, about 4 minutes. Taste for seasoning, and add salt if necessary.
-
-4. Turn off heat, add remaining butter and parsley, and toss and stir spaghetti vigorously to incorporate parsley and emulsify butter into sauce. Divide pasta among four serving plates, spooning sauce and clams evenly over each portion. Garnish with celery leaves, and serve immediately, passing lemon wedges, for squeezing, at the table.`,
-      rating: 3
+      ingredients: "",
+      directions: "",
+      rating: 1,
+      time: 0,
+      cost: 0
     };
   }
 };
@@ -96,9 +92,8 @@ Lemon wedges, for serving`,
 <style lang="scss">
 .recipie__image {
   grid-area: recipie_image;
-  background: url("https://images.pexels.com/photos/45202/brownie-dessert-cake-sweet-45202.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260")
-    no-repeat center;
   background-size: cover;
+  min-height: 400px;
 }
 section.blank {
   grid-area: blank_area;
